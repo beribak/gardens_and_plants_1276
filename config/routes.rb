9aec500cd353daa676e45717656026642802e3da
danko beribak <beribakdanko@gmail.com>
2023-07-29 01:22:01 +0200

    second commmit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :gardens do 
-    resources :plants, only: [:create] do  
+    resources :plants, only: [:create]  
   end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
